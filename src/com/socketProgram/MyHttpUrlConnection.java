84fcc767f0b9d58ec1e9881d81393d57bf179e60
Deepak <ideepakrabindra@gmail.com>
2023-03-17 12:36:59 +0530

    Updated Program

--- src/com/socketProgram/MyHttpUrlConnection.java
+++ src/com/socketProgram/MyHttpUrlConnection.java
@@ -4,9 +4,9 @@ import java.net.*;
 public class MyHttpUrlConnection {
     public static void main(String[] args) {
         try{
-            URL url =  new URL("http://www.govtenggcollegedaman.org.in/TPO_s%20Message.html");
+            URL url =  new URL("http://www.govtenggcollegedaman.org.in");
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
-            for (int i = 1; i <=9 ; i++) {
+            for (int i = 1; i <=8 ; i++) {
                 System.out.println(huc.getHeaderFieldKey(i)+" - "+huc.getHeaderField(i));
             }
             huc.disconnect();
