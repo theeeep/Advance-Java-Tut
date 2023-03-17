4d6feb484fd7da1fe62b367aeda81ff66c8823a6
Deepak <ideepakrabindra@gmail.com>
2023-03-17 09:44:21 +0530

    Inet Class Refactor

--- src/com/socketProgram/MyInet.java
+++ src/com/socketProgram/MyInet.java
@@ -7,14 +7,21 @@ import java.util.Arrays;
 public class MyInet {
     public static void main(String[] args) {
         try {
-            InetAddress ip = InetAddress.getByName("www.govtenggcollegedaman.org.in");
+            InetAddress address = InetAddress.getByName("www.govtenggcollegedaman.org.in");
 
-            System.out.println("ip: "+ip);
-            System.out.println("Address: "+ Arrays.toString(ip.getAddress()));
+            System.out.println("Host Name with Ip Address: "+address);
+            System.out.println("Address: "+ Arrays.toString(address.getAddress()));
 
-            System.out.println("Host Name: "+ip.getHostName());
-            System.out.println("Host Address: "+ip.getHostAddress());
-            System.out.println("Hash Code: "+ip.hashCode());
+            System.out.println("Host Name: "+address.getHostName());
+            System.out.println("Host Address: "+address.getHostAddress());
+            System.out.println("Hash Code: "+address.hashCode());
+
+
+            InetAddress adress2[] = InetAddress.getAllByName("www.govtenggcollegedaman.org.in");
+            for (int i = 0; i < adress2.length; i++) {
+                System.out.println(adress2[i]);
+            }
+
         } catch (Exception e) {
             System.out.println(e);
         }
