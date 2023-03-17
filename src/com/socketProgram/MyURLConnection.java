b441094c48c5e1c905cc6436c89c536eef205128
Deepak <ideepakrabindra@gmail.com>
2023-03-17 11:30:17 +0530

    Updated Program

--- src/com/socketProgram/MyURLConnection.java
+++ src/com/socketProgram/MyURLConnection.java
@@ -1,21 +1,18 @@
 package com.socketProgram;
 
-import java.io.*;
+
 import java.net.*;
 import java.util.Date;
 
 public class MyURLConnection {
     public static void main(String[] args) {
         try{
-//            URL url  = new URL("http://www.govtenggcollegedaman.org.in/TPO_s%20Message.html");
             URL url  = new URL("https://www.google.com");
             URLConnection urlConnection = url.openConnection();
-//            InputStream stream = urlConnection.getInputStream();
 
             System.out.println("The content of given url is:" + urlConnection.getContent());
             String ContentType = urlConnection.getContentType();
             System.out.println("The Content type is: "+ContentType);
-
 
             long date = urlConnection.getDate();
             if (date == 0){
@@ -30,15 +27,8 @@ public class MyURLConnection {
             }else {
                 System.out.println("Expiry Date: "+new Date(ExpiryDate));
             }
-
-
-//            int l;
-//            while ((l=stream.read()) !=-1){
-//                System.out.print((char)l);
-//            }
         }catch(Exception e){
             System.out.println(e);
         }
-
     }
 }
